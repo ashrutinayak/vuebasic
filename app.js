80be9b755af9b216f1cee83eea8ca425ea670a75
weidong <weidongchen.auctionsoftware@gmail.com>
2019-12-06 15:57:20 +0530

    basic for vue instance and how to create dynamic url

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data: {
+        name: 'X person',
+        website: 'https://www.tutorialspoint.com/',
+        websiteurl: 'https://www.freeformatter.com/',
+        websiteTag: '<a href="https://vuejs.org/">Vue JS</a>'
+    },
+    methods: {
+        sum: function (num1, num2) {
+            // var ans;
+            return ans = this.name + ' salary is : $' + (num1 + num2);
+        }
+    }
+});
