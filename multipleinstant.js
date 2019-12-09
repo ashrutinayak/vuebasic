3a156a59040c59b4a8bd48d8cce20ede8a4b88a8
weidong <weidongchen.auctionsoftware@gmail.com>
2019-12-09 14:48:31 +0530

    multiple instance

---
+++ multipleinstant.js
@@ -0,0 +1,28 @@
+var one = new Vue({
+    el: "#pra",
+    data: {
+        name: ['Ashu', 'RK', 'AN', 'AP'],
+        title: 'demo'
+
+    },
+    methods: {
+
+
+    },
+    computed: {
+
+    }
+});
+var two = new Vue({
+    el: "#test",
+    data: {
+        name: ['Ashu1', 'RK1', 'AN1', 'AP1']
+    },
+    methods: {
+
+
+    },
+    computed: {
+
+    }
+});
